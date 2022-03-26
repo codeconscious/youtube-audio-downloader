351aa98d8332b18ab2e7d02c5d605c324060f627
CodeConscious <hello@codeconscio.us>
2022-03-26 18:25:06 +0900

    First code commit

---
+++ Youtube-Downloader/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Youtube_Downloader
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
