902076d1cc0e70738da61e533c4ccdaa596c6ee8
CodeConscious <hello@codeconscio.us>
2022-03-26 21:12:31 +0900

    Downloading works

--- Youtube-Downloader/MainWindow.axaml.cs
+++ Youtube-Downloader/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -13,8 +14,49 @@ namespace Youtube_Downloader
         private void OnButtonClick(object sender, RoutedEventArgs e)
         {
             var button = (Button) sender;
+            var urlTextBox = this.FindControl<TextBox>("Url");
+            var url = urlTextBox.Text.Trim();
+            if (url.Length < 11)
+            {
+                button.Content = "Too short!";
+                return;
+            }
+            else if (url.Length > 11)
+            {
+                button.Content = "Too long!";
+                return;
+            }
 
-            button.Content = "OK";
+            button.Content = "Working...";
+
+            // Adapted from https://stackoverflow.com/a/1469790/11767771:
+            // var process = new System.Diagnostics.Process();
+            // var startInfo = new System.Diagnostics.ProcessStartInfo();
+            // startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            // startInfo.FileName = "cmd.exe"; // Windows only
+            // startInfo.Arguments = $"/C yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 \"https://www.youtube.com/watch?v={url}\"";
+            // process.StartInfo = startInfo;
+            // process.Start();
+            // process.WaitForExit();
+            // var exitCode = process.ExitCode;
+            // button.Content = $"Done (code {exitCode})";
+
+            // Mac-friendly version from https://stackoverflow.com/a/65676526/11767771:
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = "yt-dlp",
+                Arguments = $"--extract-audio --audio-format mp3 --audio-quality 0 \"https://www.youtube.com/watch?v={url}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                WorkingDirectory = "/Users/jd/Downloads/Music"
+            };
+
+            var process = Process.Start(processInfo);
+            process.WaitForExit();
+            button.Content = process.ExitCode == 0
+                ? "Done (OK)"
+                : $"Done (ERROR: {process.ExitCode})";
         }
     }
 }
