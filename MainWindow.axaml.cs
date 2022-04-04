394733b48e5c06211a5da12c44662bc9b4d56cbc
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2022-04-04 17:08:53 +0900

    Add file renaming

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -54,13 +56,19 @@ namespace Youtube_Downloader
             }
             if (downloadExitCodeOrNull == 0) // Success
             {
-                log.Text += "Saved OK!";
+                log.Text += "Saved OK!\n";
                 urlPartTextBox.Text = string.Empty;
             }
             else
             {
                 log.Text += $"ERROR: Could not download the video (error code {downloadExitCodeOrNull.ToString() ?? "NULL"})\n\n";
             }
+
+            var newFileName = this.FindControl<TextBox>("FileName");
+            var saveFolderTextBox = this.FindControl<TextBox>("SaveFolder");
+            if (string.IsNullOrWhiteSpace(newFileName?.Text))
+                return;
+            RenameFile(videoId, saveFolderTextBox.Text, newFileName.Text);
         }
 
         private int? DownloadVideo(string videoId)
@@ -138,9 +146,49 @@ namespace Youtube_Downloader
             return process.ExitCode;
         }
 
-        private void RenameFile(string videoId, string path)
+        private void RenameFile(string videoId, string directory, string newFileName)
         {
-            //var a = System.IO.Directory.EnumerateFiles(path, $"*{videoId}*");
+            GuardClauses();
+
+            var log = this.FindControl<TextBlock>("Log");
+
+            log.Text += $"Renaming file with video ID \"{videoId}\" to \"{newFileName}\"...\n";
+
+            var foundFiles = Directory.EnumerateFiles(directory, $"*{videoId}*").ToList();
+
+            if (foundFiles.Count == 0)
+            {
+                log.Text += $"No file to rename was found in \"{directory}\"\n";
+                return;
+            }
+
+            if (foundFiles.Count > 1)
+            {
+                // Error, and print filenames
+                log.Text += $"{foundFiles.Count} files containing \"{videoId}\" in their names were found:\n";
+                foundFiles.ForEach(f => log.Text += "- " + f + "\n");
+                return;
+            }
+
+            try
+            {
+                File.Move(foundFiles[0],
+                          Path.Combine(directory, newFileName) + Path.GetExtension(foundFiles[0]),
+                          overwrite: false);
+            }
+            catch (Exception ex)
+            {
+                 log.Text += $"RENAMING ERROR: " + ex.Message + "\n";
+                 return;
+            }
+
+            log.Text += "Rename OK!\n";
+
+            void GuardClauses()
+            {
+                if (string.IsNullOrWhiteSpace(videoId))
+                    throw new InvalidOperationException();
+            }
         }
     }
 }
